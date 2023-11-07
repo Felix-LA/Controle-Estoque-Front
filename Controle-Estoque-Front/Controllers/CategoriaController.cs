74a474729924ccd26f0cdd39ad03b669be5f9eae
Felix <felix.lobo85@gmail.com>
2023-11-07 14:43:48 -0400

    Adicionando Categoria e tabelas

---
+++ Controle-Estoque-Front/Controllers/CategoriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_Estoque_Front.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
